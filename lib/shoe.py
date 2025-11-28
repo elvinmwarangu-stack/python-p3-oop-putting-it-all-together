2139753c6de407da66d6caabd93571239c92973c
Elvin <elvin.mwarangu@student.moringaschool.com>
2025-11-28 23:03:42 +0300

    done solution

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size   # This will now pass through the setter
+
+    @property
+    def size(self):
+        """Return the size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        """Set the size property"""
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
